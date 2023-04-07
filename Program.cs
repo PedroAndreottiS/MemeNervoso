4d20cc732d1362a20970da63d196b15bbb52104d
Pedro Andreotti Sales <pedroandreottijetforce@gmail.com>
2023-04-07 13:50:54 -0300

    Foi uma licao bem...NAO, PERA. To nervoso.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ string opiniao;
 
 Console.Write("Qual é a sua opinião sobre esse programa?");
 opiniao = Console.ReadLine()!;
-Console.WriteLine($"{opiniao.Remove(10)}...NÃO, PERA. \n Tô nervoso");
+Console.WriteLine("E sabe o que EU acho de você?");
+Console.WriteLine($"{opiniao.Remove(10)}...NÃO, PERA. \n Tô nervoso!");
 
-Console.ForegroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Blue;
 
 
 
