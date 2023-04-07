d408878d54d6a75aa08494e139b85b020a6d9c8f
PEDRO ANDREOTTI SALES <pedroandreotttijetforce@gmail.com>
2023-04-07 13:31:47 -0300

    Foi uma licao bem...NAO, PERA. To nervoso.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string opiniao;
+
+Console.Write("Qual é a sua opinião sobre esse programa?");
+opiniao = Console.ReadLine()!;
+Console.WriteLine($"{opiniao.Remove(10)}...NÃO, PERA. \n Tô nervoso");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+
+
+
